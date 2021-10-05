357a1c10b7e46236813f568a38524e78ae3a2636
AlaaTharwat <56792090+AlaaTharwat@users.noreply.github.com>
2021-10-05 11:10:52 +0200

    change in products page

--- components/agility-pageModules/ProductsListing.js
+++ components/agility-pageModules/ProductsListing.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 import { AgilityImage } from "@agility/nextjs"
 
 const ProductsListing = ({ module, customData }) => {
-  // get posts
-  const { posts } = customData;
+  // get products
+  const { products } = customData;
 
 
-  // if there are no posts, display message on frontend
-  if (posts.length <= 0) {
+  // if there are no products, display message on frontend
+  if (products.length <= 0) {
     return (
       <div className="mt-44 px-6 flex flex-col items-center justify-center">
-        <h1 className="text-3xl text-center font-bold">No posts available.</h1>
+        <h1 className="text-3xl text-center font-bold">No products available.</h1>
         <div className="my-10">
           <Link href={"/"}>
             <a className="px-4 py-3 my-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-primary-600 hover:bg-primary-500 focus:outline-none focus:border-primary-700 focus:shadow-outline-primary transition duration-300">
@@ -27,28 +27,28 @@ const ProductsListing = ({ module, customData }) => {
     <div className="relative px-8 mb-12">
       <div className="max-w-screen-xl mx-auto">
         <div className="sm:grid sm:gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          {posts.map((post, index) => (
-            <Link href={post.url} key={index}>
+          {products.map((product, index) => (
+            <Link href={product.url} key={index}>
               <a>
                 <div className="flex-col group mb-8 md:mb-0">
                   <div className="relative h-64">
                     <AgilityImage
-                      src={post.imageSrc}
-                      alt={post.imageAlt}
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
                       className="object-cover object-center rounded-t-lg"
                       layout="fill"
                     />
                   </div>
                   <div className="bg-gray-100 p-8 border-2 border-t-0 rounded-b-lg">
                     <div className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose">
-                      {post.category}
+                      {product.category}
                     </div>
                     <div className="border-b-2 border-primary-500 w-8"></div>
                     <div className="mt-4 uppercase text-gray-600 italic font-semibold text-xs">
-                      {post.date}
+                      {product.date}
                     </div>
                     <h2 className="text-secondary-500 mt-1 font-black text-2xl group-hover:text-primary-500 transition duration-300">
-                      {post.title}
+                      {product.title}
                     </h2>
                   </div>
                 </div>
@@ -61,13 +61,13 @@ const ProductsListing = ({ module, customData }) => {
   );
 };
 
-// function to resole post urls
-const resolvePostUrls = function (sitemap, posts) {
+// function to resole product urls
+const resolvePostUrls = function (sitemap, products) {
   let dynamicUrls = {};
-  posts.forEach((post) => {
+  products.forEach((product) => {
     Object.keys(sitemap).forEach((path) => {
-      if (sitemap[path].contentID === post.contentID) {
-        dynamicUrls[post.contentID] = path;
+      if (sitemap[path].contentID === product.contentID) {
+        dynamicUrls[product.contentID] = path;
       }
     });
   });
@@ -89,9 +89,9 @@ ProductsListing.getCustomInitialProps = async ({
       languageCode,
     });
 
-    // get posts...
-    let rawPosts = await api.getContentList({
-      referenceName: "posts",
+    // get products...
+    let rawProducts = await api.getContentList({
+      referenceName: "products",
       languageCode,
 	  contentLinkDepth: 2,
 	  depth: 2,
@@ -99,27 +99,27 @@ ProductsListing.getCustomInitialProps = async ({
     });
 
     // resolve dynamic urls
-    const dynamicUrls = resolvePostUrls(sitemap, rawPosts.items);
+    const dynamicUrls = resolvePostUrls(sitemap, rawProducts.items);
 
-    const posts = rawPosts.items.map((post) => {
+    const products = rawProducts.items.map((product) => {
       //category
-      const category = post.fields.category?.fields.title || "Uncategorized"
+      const category = product.fields.category?.fields.title || "Uncategorized"
 
       // date
-      const date = new Date(post.fields.date).toLocaleDateString();
+      const date = new Date(product.fields.date).toLocaleDateString();
 
       // url
-      const url = dynamicUrls[post.contentID] || "#";
+      const url = dynamicUrls[product.contentID] || "#";
 
-      // post image src
-      let imageSrc = post.fields.image.url;
+      // product image src
+      let imageSrc = product.fields.image.url;
 
-      // post image alt
-      let imageAlt = post.fields.image?.label || null;
+      // product image alt
+      let imageAlt = product.fields.image?.label || null;
 
       return {
-        contentID: post.contentID,
-        title: post.fields.title,
+        contentID: product.contentID,
+        title: product.fields.title,
         date,
         url,
         category,
@@ -129,7 +129,7 @@ ProductsListing.getCustomInitialProps = async ({
     });
 
     return {
-      posts,
+      products,
     };
   } catch (error) {
     if (console) console.error(error);
