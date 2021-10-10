b6c7e2e5a218b70c300b4915113d102aa9717a43
alaaTharwat <alaa.tharrwat@gmail.com>
2021-10-10 12:04:47 +0200

    Adding products API

---
+++ pages/api/products.js
@@ -0,0 +1,16 @@
+import agility from '@agility/content-fetch'
+
+export default async (req, res) => {
+    const api = agility.getApi({
+        guid: process.env.AGILITY_GUID,
+        apiKey: process.env.AGILITY_API_PREVIEW_KEY,
+        isPreview: true
+    })
+
+    const products = await api.getApi({
+        referenceName: 'products',
+        languageCode: 'en-us'
+    })
+
+    res.status(200).json(products)
+}
